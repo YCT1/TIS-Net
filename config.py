dfd82e3770050daa234cca2a044a7c5c31d2b96a
Yekta Can Tursun <yektacan1999@gmail.com>
2021-11-24 22:48:05 +0300

    config update

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # Train on simulated data (S: normal random dist.) or external data as square matrix (E)
 # Or if your data is already horizontally stacked feature vectors use (E2)
-DATASET = "S"
+DATASET = "E2"
 
 # Path to external source data (binary file in NumPy .npy format)
 # Dimension of source data: [N_SUBJECTS, N_SOURCE_NODES, N_SOURCE_NODES]
 # Ignored if DATASET = "S"
-SOURCE_DATA_PATH = "external/source/source_data.npy"
+SOURCE_DATA_PATH = "external/source/source.npy"
 
 # Path to external target data (binary file in NumPy .npy format)
 # Dimension of target data: [N_SUBJECTS, N_TARGET_NODES, N_TARGET_NODES]
 # Ignored if DATASET = "S"
-TARGET_DATA_PATH = "external/target/target_data.npy"
+TARGET_DATA_PATH = "external/target/target.npy"
 
 # Number of subjects in simulated data (Overwritten if DATASET = "E")
 N_SUBJECTS = 100
@@ -22,7 +22,7 @@ N_SOURCE_NODES = 35
 N_TARGET_NODES = 160
 
 # Number of traning epochs
-N_EPOCHS = 5
+N_EPOCHS = 400
 
 # Number of folds for cross validation
 N_FOLDS = 3
